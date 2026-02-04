bc1af80b3e5bea2c3095c2e165b5d3dd23c500a6
juwineza <juwineza@gaming.tech>
2026-02-04 12:55:02 +0100

    Added PlayerMoveState

--- FiniteStateMachine/PlayerMoveState.cpp
+++ FiniteStateMachine/PlayerMoveState.cpp
@@ -1,6 +1,17 @@
+#include <iostream>
 #include "PlayerMoveState.h"
+
+void PlayerMoveState::Enter(PlayerContext _context)
+{
+	std::cout << "Enter Move State" << std::endl;
+}
+
+void PlayerMoveState::Exit(PlayerContext _context)
+{
+	std::cout << "Exit Move State" << std::endl;
+}
 
 void PlayerMoveState::Execute(PlayerContext _context)
 {
-
+	ApplyMouvement(_context, _context.moveInputX, _context.moveInputY, 200.0f);
 }
