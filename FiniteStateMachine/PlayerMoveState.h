01261b1ce2c50381edd4300abac90fdbb443fca9
juwineza <juwineza@gaming.tech>
2026-02-04 12:55:02 +0100

    Added PlayerMoveState

--- FiniteStateMachine/PlayerMoveState.h
+++ FiniteStateMachine/PlayerMoveState.h
@@ -4,6 +4,8 @@
 class PlayerMoveState : public PlayerBaseState
 {
 public:
+	void Enter(PlayerContext _context) override;
+	void Exit(PlayerContext _context) override;
 	void Execute(PlayerContext _context) override;
 };
 
